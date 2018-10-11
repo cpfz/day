fbf5178ba68858634ec34d6ef4a4b0f488ff2c58
cpfz <850244220@qq.com>
2018-10-11 09:34:54 +0800

    commit

---
+++ src/com/cpf/test/A.java
@@ -0,0 +1,8 @@
+package com.cpf.test;
+
+/**
+ * Created by Administrator on 2018/10/11 0011.
+ */
+public interface A {
+    int x=0;
+}
---
+++ src/com/cpf/test/Demo.java
@@ -0,0 +1,10 @@
+package com.cpf.test;
+
+/**
+ * Created by Administrator on 2018/10/11 0011.
+ */
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+}
